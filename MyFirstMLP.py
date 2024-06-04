bb0aa3fe17d53580fd79213721745a41b2d967f8
JYDAG <junyugao0513@gmail.com>
2024-06-04 15:57:10 +0800

    testcode

---
+++ MyFirstMLP.py
@@ -0,0 +1,53 @@
+import torch
+from torch import nn
+
+#这里用torch.nn实现一个MLP
+class MLP(nn.Module):
+    def __init__(self, in_dim, hid_dim1, hid_dim2, out_dim):
+        super(MLP, self).__init__()
+        self.layer = nn.Sequential(
+          nn.Linear(in_dim, hid_dim1),
+          nn.ReLU(),
+          nn.Linear(hid_dim1, hid_dim2),
+          nn.ReLU(),
+          nn.Linear(hid_dim2, out_dim),
+          nn.ReLU()
+       )
+    def forward(self, x):
+        x = self.layer(x)
+        return x
+
+batch_n = 100#一个批次输入数据的数量
+hidden_layer = 100
+input_data = 1000#每个数据的特征为1000
+output_data = 10
+
+x = torch.randn(batch_n,input_data)
+y = torch.randn(batch_n,output_data)
+
+w1 = torch.randn(input_data,hidden_layer)
+w2 = torch.randn(hidden_layer,output_data)
+
+epoch_n = 20
+lr = 1e-6
+
+for epoch in range(epoch_n):
+    h1=x.mm(w1)#(100,1000)*(1000,100)-->100*100
+    print(h1.shape)
+    h1=h1.clamp(min=0)
+    y_pred = h1.mm(w2)
+    
+    loss = (y_pred-y).pow(2).sum()
+    print("epoch:{},loss:{:.4f}".format(epoch,loss))
+    
+    grad_y_pred = 2*(y_pred-y)
+    grad_w2 = h1.t().mm(grad_y_pred)
+    
+    grad_h = grad_y_pred.clone()
+    grad_h = grad_h.mm(w2.t())
+    grad_h.clamp_(min=0)#将小于0的值全部赋值为0，相当于sigmoid
+    grad_w1 = x.t().mm(grad_h)
+    
+    w1 = w1 -lr*grad_w1
+    w2 = w2 -lr*grad_w2
+
